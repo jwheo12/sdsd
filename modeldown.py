530a658b3302d79ec64cf3d6bdd3c16ced34fd9b
jwheo12 <123968929+jwheo12@users.noreply.github.com>
2023-05-31 11:58:35 +0900

    Update modeldown.py

--- modeldown.py
+++ modeldown.py
@@ -1,7 +1,11 @@
 import gdown
 
-file_ids = ["1NcOMBevPqxKMDnWkLX4rkXBVXGZtS7wL"]
-file_names = ["xRicaMix_v5.6_2.5D.safetensors"]
+file_ids = ["1NcOMBevPqxKMDnWkLX4rkXBVXGZtS7wL",
+            "1mOsJK_RqmD2eSUPIRbMjROA-9yaeu0vA",
+            "1QmbsH-W9-fvDEyAxma0uNekxkflrMe5i"]
+file_names = ["xRicaMix_v5.6_2.5D.safetensors",
+              "henmixReal_v22.safetensors",
+              "chilloutmix_NiPrunedFp32Fix.safetensors"]
 save_dir = "/workspace/sdsd/stable-diffusion-webui/models/Stable-diffusion"
 
 base_url = "https://drive.google.com/uc?id="
