0fc2804498a3e6edf74fc3bdedbde6773bb05967
jwheo12 <123968929+jwheo12@users.noreply.github.com>
2023-05-31 12:00:10 +0900

    Update loradown.py

--- loradown.py
+++ loradown.py
@@ -1,1 +1,11 @@
-c
+import gdown
+
+file_ids = [""]
+file_names = [""]
+save_dir = "/workspace/sdsd/stable-diffusion-webui/models/Stable-diffusion"
+
+base_url = "https://drive.google.com/uc?id="
+for file_id, file_name in zip(file_ids, file_names):
+    url = base_url + file_id
+    output = f"{save_dir}/{file_name}"
+    gdown.download(url, output, quiet=False)
