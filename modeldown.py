a160b3c2d27e18e5b963a2475d17e8021cf9794f
jwheo12 <123968929+jwheo12@users.noreply.github.com>
2023-06-01 21:49:27 +0900

    Update modeldown.py

--- modeldown.py
+++ modeldown.py
@@ -1,3 +1,4 @@
+import os
 import gdown
 
 file_ids = ["1NcOMBevPqxKMDnWkLX4rkXBVXGZtS7wL",
@@ -12,4 +13,10 @@ base_url = "https://drive.google.com/uc?id="
 for file_id, file_name in zip(file_ids, file_names):
     url = base_url + file_id
     output = f"{save_dir}/{file_name}"
+    
+    # 파일이 이미 존재하는지 확인
+    if os.path.exists(output):
+        print(f"Skipping {file_name} - File already exists")
+        continue
+    
     gdown.download(url, output, quiet=False)
